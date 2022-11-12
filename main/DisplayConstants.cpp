778dff39a88e76de1261bdbef32feaf7b0f12f92
Josh McCullough <joshua.mccullough@gmail.com>
2022-11-12 11:08:03 -0500

    WIP

--- main/DisplayConstants.cpp
+++ main/DisplayConstants.cpp
@@ -2,17 +2,15 @@
 #include <TM1637Display.h> 
 #include "DisplayConstants.h"
 
-void animate(const uint8_t frames[][4], const uint8_t frameCount, const TM1637Display *disp, const uint16_t timeMS, const uint8_t iterations) {
-  const uint16_t interval = (timeMS / iterations) / frameCount;
+void animate(const uint8_t frames[][4], const uint8_t frameCount, const TM1637Display *disp, const uint16_t timeMS) {
+  const uint16_t interval = timeMS / frameCount;
 
-  for (uint8_t i = 0; i < iterations; i++) {
-    for (uint8_t f = 0; f < frameCount; f++) {
-      disp->setSegments(frames[f]);
-      delay(interval);
-    }
+  for (uint8_t f = 0; f < frameCount; f++) {
+    disp->setSegments(frames[f]);
+    delay(interval);
   }
 }
 
-void animateLoopClockwise(const TM1637Display *disp, const uint16_t timeMS, const uint8_t iterations) {
-  animate(ANIM_LOOP_CW, 12, disp, timeMS, iterations);
+void animateLoopClockwise(const TM1637Display *disp, const uint16_t timeMS) {
+  animate(ANIM_LOOP_CW, 12, disp, timeMS);
 }
